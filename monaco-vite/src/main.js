93bebdb39b68567807457318113b0d52cc72a8c3
Byamba <ByambaOchir.Oyungerel@gmail.com>
2025-04-01 20:31:59 +0200

    deleted files

--- monaco-vite/src/main.js
+++
@@ -1,7 +0,0 @@
-console.log("hello1");
-import {PositionListener} from "./static/parser.js";
-console.log("hello2");
-import {setupMonaco} from "./static/monaco.js";
-
-console.log("this was called in index!");
-setupMonaco();
